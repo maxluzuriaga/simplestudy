6a891f389ffbc894e945598fc732cc8a19b96865
Max Luzuriaga <max@luzuriaga.com>
2014-04-29 18:38:13 -0400

    Added users migration.

--- migrations/20140429220651-add-users.js
+++ migrations/20140429220651-add-users.js
@@ -2,9 +2,17 @@ var dbm = require('db-migrate');
 var type = dbm.dataType;
 
 exports.up = function(db, callback) {
-
+  db.createTable('users', {
+    id: {
+      type: 'serial',
+      primaryKey: true
+    },
+    email: 'string',
+    fullName: 'string',
+    authorizationToken: 'string'
+  }, callback);
 };
 
 exports.down = function(db, callback) {
-
+  db.dropTable('users', callback);
 };

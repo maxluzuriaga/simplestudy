25002098b141d7c0e74f84189d7f7686f3048d04
Max Luzuriaga <max@luzuriaga.com>
2014-04-30 11:36:04 -0400

    Added migrations for guides and sections.

---
+++ migrations/20140430152910-add-guides.js
@@ -0,0 +1,18 @@
+var dbm = require('db-migrate');
+var type = dbm.dataType;
+
+exports.up = function(db, callback) {
+  db.createTable('guides', {
+    id: {
+      type: 'serial',
+      primaryKey: true
+    },
+    name: 'string',
+    createdDate: 'timestamptz',
+    owner_id: 'int'
+  }, callback);
+};
+
+exports.down = function(db, callback) {
+  db.dropTable('guides', callback);
+};
---
+++ migrations/20140430152913-add-sections.js
@@ -0,0 +1,20 @@
+var dbm = require('db-migrate');
+var type = dbm.dataType;
+
+exports.up = function(db, callback) {
+  db.createTable('sections', {
+    id: {
+      type: 'serial',
+      primaryKey: true
+    },
+    text: 'string',
+    approved: 'boolean',
+    editedDate: 'timestamptz',
+    guide_id: 'int',
+    user_id: 'int'
+  }, callback);
+};
+
+exports.down = function(db, callback) {
+
+};

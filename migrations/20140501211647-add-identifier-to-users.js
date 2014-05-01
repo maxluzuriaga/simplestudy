1fa039fb553f999631a29a8597c219709a3806b9
Max Luzuriaga <max@luzuriaga.com>
2014-05-01 17:18:25 -0400

    Added identifier to users table.

---
+++ migrations/20140501211647-add-identifier-to-users.js
@@ -0,0 +1,12 @@
+var dbm = require('db-migrate');
+var type = dbm.dataType;
+
+exports.up = function(db, callback) {
+  db.addColumn('users', 'identifier', {
+    type: 'string'
+  }, callback);
+};
+
+exports.down = function(db, callback) {
+  db.removeColumn('users', 'identifier', callback);
+};

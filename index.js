240bf3b19e84aa515cb8727353d0dcd0f4a8b72d
Max Luzuriaga <max@luzuriaga.com>
2014-05-01 17:40:12 -0400

    Fixed issue with database connection happening after model initialization.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
+require('./lib/db').connect();
+
 var server = require ("./lib/server"),
-    db = require("./lib/db"),
     routes = require("./config/routes");
 
-db.connect();
 server.start(routes, __dirname);

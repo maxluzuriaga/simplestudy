a37403a5639df361c6ff934c8e53642c7a232afb
Max Luzuriaga <max@luzuriaga.com>
2014-05-01 15:47:07 -0400

    Initial authentication with google, no creating or logging in of users yet.

--- config/routes.js
+++ config/routes.js
@@ -1,4 +1,6 @@
-var express = require('express');
+var express = require('express'),
+    passport = require('passport'),
+    GoogleStrategy = require('passport-google').Strategy;
 
 var guides_controller = require('../app/controllers/guides_controller'),
     pages_controller = require('../app/controllers/pages_controller'),
@@ -8,10 +10,28 @@ var helper = require('../lib/helper');
 
 // http://scotch.io/tutorials/javascript/learn-to-use-the-new-router-in-expressjs-4
 
+passport.use(new GoogleStrategy(
+  {
+    returnURL: 'http://localhost:5000/auth/return',
+    realm: 'http://localhost:5000/'
+  },
+  function(identifier, profile, done) {
+    console.log(identifier);
+    console.log(profile);
+    user = {};
+    done(null, user);
+  }
+));
+
 function routes(app) {
   app.get('/', pages_controller.index);
 
-  app.get('/authlandingpage', users_controller.authorize);
+  app.get('/auth', passport.authenticate('google'));
+  app.get('/auth/return', passport.authenticate('google', {
+    session: false,
+    successRedirect: '/',
+    failureRedirect: '/failureb'
+  }));
 
   var apiRouter = express.Router();
 

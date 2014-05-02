2720de904705f64b71daa709c7a9620439024946
Max Luzuriaga <max@luzuriaga.com>
2014-05-02 19:22:25 -0400

    Guide create action, along with creating and relating sections and users assigned to those sections.

--- app/controllers/guides_controller.js
+++ app/controllers/guides_controller.js
@@ -1,5 +1,10 @@
+var User = require('../models/user')
+    Guide = require('../models/guide'),
+    Section = require('../models/section');
+
 function index(request, response) {
   // all guides associated with logged-in user
+  response.json(200, request.user.toJSON());
 }
 
 function show(request, response) {
@@ -7,8 +12,60 @@ function show(request, response) {
   // don't include section texts unless user is owner or has approved section in guide
 }
 
+function _createSections(arr, index, guideid, callback) {
+  if (index >= arr.length) {
+    callback(true);
+    return;
+  }
+
+  var info = arr[index];
+  new User({email:info.email}).fetch().then(function(user) {
+    if (user) {
+      var section = new Section({
+        user_id: user.id,
+        guide_id: guideid,
+        name: info.name,
+        edited_date: new Date()
+      });
+
+      section.save().then(function(section) {
+        if (section) {
+          _createSections(arr, index+1, guideid, callback);
+        } else {
+          callback(false);
+          // throw some kind of error
+        }
+      });
+    } else {
+      callback(false);
+      // throw some kind of error
+    }
+  });
+}
+
 function create(request, response) {
-  // create a guide, return json success or failure with id of guide
+  var guide = new Guide({
+    name: request.body.name,
+    owner_id: request.user.id,
+    created_date: new Date()
+  });
+
+  guide.save().then(function(guide) {
+    if (guide) {
+      var sectioninfo = JSON.parse(request.body.sections);
+      _createSections(sectioninfo, 0, guide.id, function(success) {
+        if (success) {
+          guide.load(['owner', 'sections']).then(function(guide) {
+            response.json(200, guide.toJSON());
+          });
+        } else {
+
+        }
+      });
+    } else {
+
+    }
+  });
 }
 
 exports.index = index;

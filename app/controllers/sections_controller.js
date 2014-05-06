5c0802af4b7dc8acef24b287dd622ff3a5646575
Max Luzuriaga <max@luzuriaga.com>
2014-05-06 14:12:09 -0400

    Removed extraneous section update call.

--- app/controllers/sections_controller.js
+++ app/controllers/sections_controller.js
@@ -5,8 +5,6 @@ function update(request, response) {
   var canUpdate = (currentID == request.section.id) || (currentID == request.section.related('guide').get('owner_id'));
 
   if (canUpdate) {
-    request.section.set('text', request.body.text);
-
     request.section.save({text: request.body.text}, {patch: true}).then(function(section) {
       response.json(200, {successful: true});
     });

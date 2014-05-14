48459e792bc6bba1b938f6c64774bd418e407658
Max Luzuriaga <max@luzuriaga.com>
2014-05-14 14:00:47 -0400

    Beginning email suggestions functionality.

--- public/js/views/section-edit-view.js
+++ public/js/views/section-edit-view.js
@@ -5,7 +5,13 @@ app.SectionEditView = Backbone.View.extend({
 
 	events: {
 		"click a.delete-section": "removeSection",
-		"change input.section-name": "nameChange"
+		"change input.section-name": "nameChange",
+		"keyup input.section-email": "emailChange",
+		"blur input.section-email": "hideSuggestions"
+	},
+
+	initialize: function() {
+		this.users = new app.Users();
 	},
 
 	render: function(file) {
@@ -13,6 +19,11 @@ app.SectionEditView = Backbone.View.extend({
 		$(this.el).html(template);
 
 		return this;
+	},
+
+	sectionMoved: function() {
+		var index = $(this.el).index();
+		this.section.set('index', index);
 	},
 
 	removeSection: function(e) {
@@ -26,8 +37,26 @@ app.SectionEditView = Backbone.View.extend({
 		this.section.set('name', name);
 	},
 
-	sectionMoved: function() {
-		var index = $(this.el).index();
-		this.section.set('index', index);
+	emailChange: function() {
+		this.emailLastChanged = new Date();
+
+		var email = $(this.el).find("input.section-email").val();
+		this.section.set('email', email);
+
+		this.users.query = email;
+
+		window.setTimeout(function() {
+			if ((new Date() - this.emailLastChanged) >= 300) {
+				this.users.fetch({reset: true, success: this.showSuggestions.bind(this)});
+			}
+		}.bind(this), 300);
+	},
+
+	showSuggestions: function() {
+		console.log(this.users.models);
+	},
+
+	hideSuggestions: function() {
+		$(this.el).find(".email-list").css("visibility", "hidden");
 	}
 });

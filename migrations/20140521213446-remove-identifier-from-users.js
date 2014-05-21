3adbcb8db2391531d54bd2c9d4f58d41defcf576
Max Luzuriaga <max@luzuriaga.com>
2014-05-21 17:39:44 -0400

    Fixed stupid mistake of removing wrong field.  I'm blaming copy/paste.

--- migrations/20140521213446-remove-identifier-from-users.js
+++ migrations/20140521213446-remove-identifier-from-users.js
@@ -2,11 +2,11 @@ var dbm = require('db-migrate');
 var type = dbm.dataType;
 
 exports.up = function(db, callback) {
-  db.removeColumn('sections', 'index', callback);
+  db.removeColumn('users', 'identifier', callback);
 };
 
 exports.down = function(db, callback) {
-  db.addColumn('sections', 'index', {
-    type: 'int'
+  db.addColumn('users', 'identifier', {
+    type: 'string'
   }, callback);
 };
